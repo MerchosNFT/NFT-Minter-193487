669356aca8ca0b25ac973963f446ecb6538fc576
MerchosNFT <126315792+MerchosNFT@users.noreply.github.com>
2023-02-25 18:26:51 +1100

    Add files via upload

---
+++ precache-manifest.19d112680b0e59d523bb7f63ed4759c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5523a500ed51c7acf1d3",
+    "url": "/static/css/main.e17b0441.chunk.css"
+  },
+  {
+    "revision": "5523a500ed51c7acf1d3",
+    "url": "/static/js/main.5523a500.chunk.js"
+  },
+  {
+    "revision": "284dae56d7844ac685ff",
+    "url": "/static/js/1.284dae56.chunk.js"
+  },
+  {
+    "revision": "d1b119f7c94fc53d8b96",
+    "url": "/static/js/2.d1b119f7.chunk.js"
+  },
+  {
+    "revision": "e76bf7548ec2d73cc7d7",
+    "url": "/static/js/runtime~main.e76bf754.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  },
+  {
+    "revision": "4881a61d38df33acce5e48d3b94889bc",
+    "url": "/index.html"
+  }
+];
